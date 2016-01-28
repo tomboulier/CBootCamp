4ca51e97f99430944395e34fc34dfed397df0401
tomboulier <boulier.thomas@gmail.com>
2016-01-29 00:23:46 +0100

    1st exercise modified to print 3 strings instead of 1.

--- chap10/10_1.c
+++ chap10/10_1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAXSTRING 256
+
 void printToUpper(char str[]){
 	char c;
 	for (int i = 0; i < strlen(str); ++i)
@@ -17,10 +19,13 @@ void printToUpper(char str[]){
 }
 
 int main(int argc, char *argv[]){
-	char s[256];
+	char s1[MAXSTRING], s2[MAXSTRING], s3[MAXSTRING];
 
-	gets(s);
-	printToUpper(s);
+	printf("Enter 3 strings: ");
+	scanf("%s %s %s",s1,s2,s3);
+	printToUpper(s1);
+	printToUpper(s2);
+	printToUpper(s3);
 
 	return 0;
 }
